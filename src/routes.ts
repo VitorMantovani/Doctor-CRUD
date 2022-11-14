66c55574487962c3a6bbdca87010e4c55f837e89
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-11-14 17:08:04 -0300

    feat: Doctor post endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express"
+import { DoctorController } from "./controllers/doctorController"
 
 const routes = Router()
+
+routes.post("/doctor", new DoctorController().create)
 
 export default routes
