1adc00fc571bd48d1898f8c0c954709a900f4bdd
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-11-17 14:30:22 -0300

    feat: insert and select

--- src/database/repositories/doctorReposirory.ts
+++ src/database/repositories/doctorReposirory.ts
@@ -1,4 +1,29 @@
-import { AppDataSource } from "../data-source";
+import { AppDataSource } from "..";
 import { Doctor } from "../entities/doctor";
 
-export const doctorRepository = AppDataSource.getRepository(Doctor);
+type DoctorRequest = {
+    name: string;
+    crm: string;
+    phone: string;
+    mobilePhone: string;
+    zipCode: string;
+}
+
+export class doctorRepository {
+    
+    repository = AppDataSource.getRepository(Doctor);
+    
+    async insert({name, crm, phone, mobilePhone, zipCode}: DoctorRequest): Promise<Doctor> {
+    
+        return this.repository.save({name, crm, phone, mobilePhone, zipCode});
+
+    }
+
+    async select() {
+        return this.repository.find()
+    }
+
+}
+
+
+
