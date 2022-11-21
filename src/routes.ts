0042dc8bde677283495fbd662bbf9b3e6e2d6053
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-11-21 16:41:54 -0300

    feat: routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express"
 import { DoctorController } from "./controllers/doctorController"
+import { SpecialtyController } from "./controllers/specialtyController";
 
 const routes = Router()
 
-routes.post("/doctor", new DoctorController().create)
+routes.post("/doctor/create", new DoctorController().create);
+routes.get("/doctor/select", new DoctorController().read);
+routes.get("doctor/select/:crm", new DoctorController().readByCrm)
+routes.delete("/doctor/:crm", new DoctorController().deleteByCrm)
+routes.put("/doctor/update", new DoctorController().update)
 
-export default routes
+routes.post("/specialty/create", new SpecialtyController().create);
+
+export default routes;
